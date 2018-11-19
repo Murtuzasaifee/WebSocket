c0b573571fa2da0e88620b868511e6b5b1539487
Murtuza Saifee <ermurtuzasaifee@gmail.com>
2018-11-19 15:51:24 +0400

    Splash cum landing screen designed

--- app/src/main/java/com/bitoasis/websocket/inits/SplashScreen.java
+++ app/src/main/java/com/bitoasis/websocket/inits/SplashScreen.java
@@ -1,13 +1,15 @@
-package com.bitoasis.websocket;
+package com.bitoasis.websocket.inits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.bitoasis.websocket.R;
+
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
